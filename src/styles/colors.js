55598ae106d4f5b73f3107f5e8bd153f1e37909e
Napat Cheetanom <napatchee01@gmail.com>
2020-11-03 00:29:28 +0700

    add : color palette

---
+++ src/styles/colors.js
@@ -0,0 +1,21 @@
+const colors = {
+  default: {
+    primary: "#3F51B5",
+    secondary: "#F50057",
+    error: "#F44336",
+    warning: "#FF9800",
+    info: "#2196F3",
+    success: "#4CAF50"
+  },
+
+  /* cutomized pallete */
+  darkBlue : "#14274E" ,
+  blue : "394867" ,
+  darkPink: "#D46C86",
+  grey: "#C4C4C4",
+  white : "F2F2F2" ,
+  green : "5DB075" ,
+  black: "#1e1e1e",
+}
+
+export default colors
