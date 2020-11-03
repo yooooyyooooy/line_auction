8519486b11affb15ff76d1e05bb37aeb9fbbb348
Napat Cheetanom <napatchee01@gmail.com>
2020-11-03 15:01:58 +0700

    add : check mobile tablet desktop

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,8 @@ import {makeStyles} from "@material-ui/core/styles"
 import {Box,Typography} from "@material-ui/core"
 import Intersect from "../image/Intersect.svg"
 
+import {isMobile} from "react-device-detect"
+
 /* initialize fontSize for responsive mode */
 const headerFontSize = {
     xs: "1.3rem",
@@ -15,18 +17,26 @@ const headerFontSize = {
   }
 
 export default function Home() {
+    if (isMobile) {
+        return (
+            <>
+                <Navbar/>
+                <div style={{position:"relative",height:"100%"}}>
+                    <img style={{position:"absolute",zIndex:-1,top:"-25vh",maxWidth:"100%"}} src={Intersect} alt="Intersect"/>
+                </div>
+                <Box style={{marginTop:"5%",marginLeft:"7%"}}>
+                    <Typography style={{fontSize:headerFontSize.xs,color : colors.white}}>WELCOME BACK !</Typography>
+                </Box>
+                <Box style={{marginTop:"2%",marginLeft:"15%"}}>
+                    <Typography style={{fontSize:headerFontSize.md,color : colors.white}}>Poon</Typography>
+                </Box>
+            </>
+        )
+    }
     return (
         <>
             <Navbar/>
-            <div style={{position:"relative",height:"100%"}}>
-                <img style={{position:"absolute",zIndex:-1,top:"-25vh",maxWidth:"100%"}} src={Intersect} alt="Intersect"/>
-            </div>
-            <Box style={{marginTop:"5%",marginLeft:"7%"}}>
-                <Typography style={{fontSize:headerFontSize.xs,color : colors.white}}>WELCOME BACK !</Typography>
-            </Box>
-            <Box style={{marginTop:"2%",marginLeft:"15%"}}>
-                <Typography style={{fontSize:headerFontSize.md,color : colors.white}}>Poon</Typography>
-            </Box>
+            Hello World
         </>
     )
 }
