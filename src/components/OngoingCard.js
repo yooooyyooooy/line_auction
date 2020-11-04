917248d641752602f2bbee7c6a1af4d18aad475f
Napat Cheetanom <napatchee01@gmail.com>
2020-11-04 14:21:22 +0700

    fix

--- src/components/OngoingCard.js
+++ src/components/OngoingCard.js
@@ -31,19 +31,11 @@ function OngoingCard() {
             <Typography>ON-GOING</Typography>
           </Box>
         </Box>
-<<<<<<< HEAD
         <List>
           <ItemCard title="เพชรวิบวับนำเข้าจากอังกฤษ" time="เหลือเวลาอีก 20 นาที"/>
           <ItemCard title="วิบวับวิบวับ" time="เหลือเวลาอีก 20 นาที"/>
         </List>
         <GreenButton text="SHOW ALL ON-GOING" dest="/ongoing"/>
-||||||| 1fbf2ad
-        <List></List>
-        <GreenButton text="SHOW ALL ON-GOING" dest="/ongoing"/>
-=======
-        <List></List>
-        <GreenButton text="SHOW ALL ON-GOING" dest="/ongoing" />
->>>>>>> c5589d3dca91d3a883459eeb0c90451e7cf0b2d4
       </RoundPaper>
     </Box>
   );
